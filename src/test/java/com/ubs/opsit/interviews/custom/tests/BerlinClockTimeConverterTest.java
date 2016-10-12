19d44b63f51c4628678b9435f9cee416a33eb3ad
fisher <fisher377164@gmail.com>
2016-10-12 21:37:09 +0300

    Update berlin Clock Time Converter Tests

--- src/test/java/com/ubs/opsit/interviews/custom/tests/BerlinClockTimeConverterTest.java
+++ src/test/java/com/ubs/opsit/interviews/custom/tests/BerlinClockTimeConverterTest.java
@@ -1,6 +1,12 @@
 package com.ubs.opsit.interviews.custom.tests;
 
+import com.ubs.opsit.interviews.TimeConverter;
+import com.ubs.opsit.interviews.exceptions.InvalidTimeException;
+import com.ubs.opsit.interviews.impl.BerlinClockTimeConverter;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author fisher
@@ -8,8 +14,67 @@ import org.junit.Test;
  */
 public class BerlinClockTimeConverterTest {
 
+    private static TimeConverter converter;
+
+    @BeforeClass
+    public static void init() {
+        converter = new BerlinClockTimeConverter();
+    }
+
+    @Test(expected = InvalidTimeException.class)
+    public void testEmptyTime() {
+        String invalidTime = "";
+
+        converter.convertTime(invalidTime);
+    }
+
+    @Test(expected = InvalidTimeException.class)
+    public void testInvalidTimeWithoutSeconds() {
+        String invalidTime = "12:30";
+
+        converter.convertTime(invalidTime);
+    }
+
+    @Test(expected = InvalidTimeException.class)
+    public void testInvalidTime() {
+        String invalidTime = "24:00:00";
+
+        converter.convertTime(invalidTime);
+    }
+
     @Test
-    public void convertTime() throws Exception {
-        System.out.println("test");
+    public void testMidnight() throws Exception {
+        String time = "00:00:00";
+        String expectedTime = "Y\n" +
+                "OOOO\n" +
+                "OOOO\n" +
+                "OOOOOOOOOOO\n" +
+                "OOOO";
+        String actualTime = converter.convertTime(time);
+        assertEquals(expectedTime, actualTime);
+    }
+
+    @Test
+    public void testMiddleOfTheAfternoon() throws Exception {
+        String time = "13:17:01";
+        String expectedTime = "O\n" +
+                "RROO\n" +
+                "RRRO\n" +
+                "YYROOOOOOOO\n" +
+                "YYOO";
+        String actualTime = converter.convertTime(time);
+        assertEquals(expectedTime, actualTime);
+    }
+
+    @Test
+    public void testJustBeforeMidnight() throws Exception {
+        String time = "23:59:59";
+        String expectedTime = "O\n" +
+                "RRRR\n" +
+                "RRRO\n" +
+                "YYRYYRYYRYY\n" +
+                "YYYY";
+        String actualTime = converter.convertTime(time);
+        assertEquals(expectedTime, actualTime);
     }
 }
