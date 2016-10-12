6d0ef11cf8bc28f4c7d152dc714bf0875bcb556d
fisher <fisher377164@gmail.com>
2016-10-12 20:40:54 +0300

    Add time validation

---
+++ src/main/java/com/ubs/opsit/interviews/validation/TimeValidator.java
@@ -0,0 +1,23 @@
+package com.ubs.opsit.interviews.validation;
+
+import java.util.regex.Pattern;
+/**
+ * @author fisher
+ * @since 10/12/16.
+ */
+
+public class TimeValidator {
+
+    private Pattern pattern;
+
+    private static final String TIME_PATTERN =
+            "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+
+    public TimeValidator(){
+        this.pattern = Pattern.compile(TIME_PATTERN);
+    }
+
+    public boolean validate(String time){
+        return pattern.matcher(time).matches();
+    }
+}
