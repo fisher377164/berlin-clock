7f8f84332336804c2fcb64c0a37b604facaa66ce
fisher <fisher377164@gmail.com>
2016-10-12 20:43:41 +0300

    Add exceptions

---
+++ src/main/java/com/ubs/opsit/interviews/exceptions/InvalidTimeException.java
@@ -0,0 +1,13 @@
+package com.ubs.opsit.interviews.exceptions;
+
+/**
+ * @author fisher
+ * @since 10/12/16.
+ */
+public class InvalidTimeException extends RuntimeException {
+
+    public InvalidTimeException(String message) {
+        super(message);
+    }
+
+}
